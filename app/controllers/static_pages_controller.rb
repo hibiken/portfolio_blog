852b8d16f1e5626cc93bd00e5ac6b76af477bf45
Ken Hibino <ken.hibino7@gmail.com>
2015-10-18 19:43:30 -0400

    Clean up code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,4 @@ class StaticPagesController < ApplicationController
     @articles = Article.order(created_at: :desc).limit(3)
     @projects = Project.order(created_at: :desc).limit(3)
   end
-
-  def contact
-  end
 end

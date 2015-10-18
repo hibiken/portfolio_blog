970f3e91401097dd409f5c3acc20ea9cd437f77a
Ken Hibino <ken.hibino7@gmail.com>
2015-10-18 10:23:38 -0400

    Make home page dynamic with projects

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @articles = Article.order(created_at: :desc).limit(3)
+    @projects = Project.order(created_at: :desc).limit(3)
   end
 
   def contact

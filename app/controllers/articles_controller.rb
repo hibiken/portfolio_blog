ced94613857bf34f6115e010abde1c9a5306c239
Ken Hibino <ken.hibino7@gmail.com>
2015-11-28 19:16:40 -0500

    Scope to published article in home page and search results

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   end
 
   def search
-    @articles = Article.fulltext_search(params[:q]).paginate(page: params[:page])
+    @articles = Article.published.fulltext_search(params[:q]).paginate(page: params[:page])
     render :index
   end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @articles = Article.order(created_at: :desc).limit(3)
+    @articles = Article.published.limit(3)
     @projects = Project.order(created_at: :desc).limit(3)
   end
 end
